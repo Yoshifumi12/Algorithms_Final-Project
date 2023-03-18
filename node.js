654b03cb1786b8e45f75dd0de6c236836aa64f94
Yoshifumi Suzuki <97666575+Yoshifumi12@users.noreply.github.com>
2023-03-18 22:04:53 +0800

    Add files via upload

---
+++ node.js
@@ -0,0 +1,15 @@
+const { Colors } = require("react-native/Libraries/NewAppScreen");
+
+class Node {
+    row;
+    column;
+    value;
+}
+
+const setNode = (row,column,value) => {
+
+}
+
+const getNode = (row,column) => {
+
+}
