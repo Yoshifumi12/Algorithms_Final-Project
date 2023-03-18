65e506b35b2e0d3d34602b235ded0668666ff45f
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2023-03-18 22:45:40 +0800

    Board is now made through a function

--- node.js
+++ node.js
@@ -1,15 +1,35 @@
-const { Colors } = require("react-native/Libraries/NewAppScreen");
-
 class Node {
     row;
     column;
-    value;
+    value = 0;
+
+    constructor(row, column) {
+        this.row = row;
+        this.column = column;
+    }
 }
 
-const setNode = (row,column,value) => {
+
+const generateBoard = () => {
+    let board = document.getElementById('board')
+    for (let i = 0; i < 8; i++) {
+        const row = document.createElement("div");
+        row.className = "row"
+        row.id = `${i}`
+        for (let j = 0; j < 8; j++) {
+            const col = document.createElement("div");
+            col.className = "col"
+            col.id = `${j}`
+            row.appendChild(col)
+        }
+        board.appendChild(row)
+    }
+}
+
+const setNode = (row, column, value) => {
 
 }
 
-const getNode = (row,column) => {
+const getNode = (row, column) => {
 
 }
